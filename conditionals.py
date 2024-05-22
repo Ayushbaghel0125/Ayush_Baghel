6ab916a65cdddfdf6cde87aa3cb41ba5f482bbbf
Ayushbaghel0125 <ayushbaghel0125@gmail.com>
2024-05-23 00:01:58 +0530

    Add files via upload

---
+++ conditionals.py
@@ -0,0 +1,21 @@
+a = int(input('Enter Your Age: '))
+print("")
+print("Your age is :",a) 
+print("")
+# Conditional operators 
+# >, <, >=, <=, ==, !=
+print(a>18)  # Greater than 'a' 
+print(a<18)  # Less than 'a'
+print(a>=18) # Greater than or Equal to 'a'
+print(a<=18) # Less than or Equal to 'a'
+print(a==18) # Accurately Equal to 'a'
+print(a!=18) # Not Equal to 'a'
+
+
+
+print("")
+
+if(a>20):
+  print ("You can go there")
+else:
+  print('You cannot go there')
