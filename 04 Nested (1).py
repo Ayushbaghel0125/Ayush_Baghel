6eb648df0b9a9524c02cc6e769649a147ec5f8b8
Ayushbaghel0125 <ayushbaghel0125@gmail.com>
2024-06-06 20:55:52 +0530

    Add files via upload

---
+++ 04 Nested (1).py
@@ -0,0 +1,88 @@
+a = int(input('Enter a number between 1 - 100 = '))
+
+if ( a == 0 ):
+        print('This Number Is Zero')
+
+elif (a > 1001):
+    print('This number is greater than 1000')
+    
+
+elif (a <= 10):
+    print('These numbers are between 1-10')
+    
+    
+if(a > 10 and a <= 20):
+    print('These numbers are between 10-20')
+             
+
+elif(a > 20 and a <= 30):
+    print('These numbers are between 20-30')
+
+             
+if(a > 30 and a <= 40):
+    print('These numbers are between 30-40')
+                 
+
+elif(a > 40 and a <= 50):
+    print('These numbers are between 40-50')
+                    
+
+if(a > 50 and a <= 60):
+    print('These numbers are between 50-60')
+                        
+
+elif(a > 60 and a <= 70):
+    print('These numbers are between 60-70')
+                            
+
+if(a > 70 and a <= 80):
+    print('These numbers are between 70-80')
+                                
+
+elif(a > 80 and a <= 90):
+    print('These numbers are between 80-90')
+                        
+
+if (a > 90 and a <= 100):
+    print('These numbers are between 90 - 100')
+    
+
+elif(a > 100 and a <= 200): 
+    print('These numbers are between 100 - 200')
+
+
+if(a > 200 and a <= 300): 
+    print('These numbers are between 200 - 300')
+
+
+elif(a > 300 and a <= 400): 
+    print('These numbers are between 300 - 400')
+
+
+if(a > 400 and a <= 500): 
+    print('These numbers are between 400 - 500')
+
+
+elif(a > 500 and a <= 600): 
+    print('These numbers are between 500 - 600')
+
+
+if(a > 600 and a <= 700): 
+    print('These numbers are between 600 - 700')
+
+
+elif(a > 700 and a <= 800): 
+    print('These numbers are between 700 - 800')
+
+
+if(a > 800 and a <= 900): 
+    print('These numbers are between 800 - 900')
+
+
+elif(a > 900 and a <= 1000): 
+    print('These numbers are between 900 - 1000')
+
+
+
+else:
+    print('But this number is greater than 1000 for sure.' )
